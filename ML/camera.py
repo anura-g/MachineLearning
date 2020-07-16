bdc72c873725ae4cf77ba813acb934671997cd35
anurag <anuragmathur@hotmail.co.uk>
2020-07-16 17:30:02 +0100

    t

---
+++ ML/camera.py
@@ -0,0 +1,67 @@
+import cv2
+import os
+import re
+import numpy as np
+from PIL import Image
+import torch
+from torchvision import transforms
+from ML.model import TransformerNet
+
+class Webcam():
+    # Get paths and set vars
+    weights_fname = "candy.pth"
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    path_to_weights = os.path.join(script_path, "models", weights_fname)
+    resolution = (640, 480)
+    # Change to GPU if desired
+    device = torch.device("cuda")
+    model = TransformerNet()
+    with torch.no_grad():
+        state_dict = torch.load(path_to_weights)
+        for k in list(state_dict.keys()):
+            if re.search(r'in\d+\.running_(mean|var)$', k):
+                del state_dict[k]
+        model.load_state_dict(state_dict)
+        model.to(device)
+
+
+    # Load PyTorch Model
+
+
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        ret, frame = self.video.read()
+
+        ret, jpeg = cv2.imencode('.jpg', frame)
+
+        return jpeg.tobytes()
+
+    def style_image(self):
+        ret, frame = self.video.read()
+        cv2_im = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        pil_im = Image.fromarray(cv2_im)
+        img = pil_im.resize(self.resolution)
+
+        # Transforms to feed to network
+        small_frame_tensor_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x.mul(255))
+        ])
+        small_frame_tensor = small_frame_tensor_transform(img)
+        small_frame_tensor = small_frame_tensor.unsqueeze(0).to(self.device)
+
+        # Run inference and resize
+        output = self.model(small_frame_tensor).cpu()
+        styled = output[0]
+        styled = styled.clone().clamp(0, 255).detach().numpy()
+        styled = styled.transpose(1, 2, 0).astype("uint8")
+        # styled_resized_frame = cv2.resize(styled, (frame.shape[0], frame.shape[1]))
+        ret, jpeg = cv2.imencode('.jpg', styled)
+        return jpeg.tobytes()
+
+
--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+from ML.camera import Webcam
 
 app = Flask(__name__)
+video_stream = Webcam()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('style_transfer.html')
+
+def generate_image(camera):
+    while True:
+        try:
+            frame = camera.get_frame()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+        except: pass
+
+def generate_styled_image(camera):
+    while True:
+        try:
+            frame = camera.style_image()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        except: pass
+
+@app.route('/real_video_feed')
+#Return the generated frames
+def real_video_feed():
+    return Response(generate_image(video_stream),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/style_video_feed')
+#Return the generated frames
+def style_video_feed():
+    return Response(generate_styled_image(video_stream),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     app.run(debug=False)
